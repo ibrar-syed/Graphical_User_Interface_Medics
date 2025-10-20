e97e3bec3fce3b5fd3c4fb04f712fe0794c3cbd9
Syed <syedhussain.ibrar@gmail.com>
2025-10-20 11:56:17 +0200

    Update pros_seg.py

--- app/pros_seg.py
+++ app/pros_seg.py
@@ -15,7 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# utils/metrics.py,...
+# utils/metrics.py,..
 
 import streamlit as st
 import numpy as np
